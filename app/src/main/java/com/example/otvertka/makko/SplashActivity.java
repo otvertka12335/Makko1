caad4a5aa470df5f48d4d2359416f4d43e4bf183
Otvertka <vkravchik@gmail.com>
2018-03-15 21:51:06 +0200

    Поправив Splash

--- app/src/main/java/com/example/otvertka/makko/SplashActivity.java
+++ app/src/main/java/com/example/otvertka/makko/SplashActivity.java
@@ -7,7 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 500;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 SplashActivity.this.startActivity(intent);
                 SplashActivity.this.finish();
             }
